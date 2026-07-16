2daad8b6da4f643869b1b24d4c74ed86a7e72f21
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-07-16 14:27:39 +0200

    Ajout de la fonction plates_of_game

--- le_compte_est_bon.py
+++ le_compte_est_bon.py
@@ -3,8 +3,17 @@
 
 import random
 
+
 def result_number():
-    return random.randint(101,999)
+    return random.randint(101, 999)
+
+
+def plates_of_game():
+    plates = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 25, 50, 75, 100]
+    return random.sample(plates, 6)
+
+
 
 
 if __name__ == '__main__':
+    print(plates_game())
