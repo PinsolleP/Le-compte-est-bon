aab385df664af0b12c27f0b9cf48c28890ede51d
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-07-16 15:26:34 +0200

    Ajout des fonction number_a et number_b

--- le_compte_est_bon.py
+++ le_compte_est_bon.py
@@ -5,27 +5,33 @@ import random
 
 
 def result_number():
+
     return random.randint(101, 999)
 
 
 def plates_of_game():
+
     plates = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 25, 50, 75, 100]
     return random.sample(plates, 6)
 
 
 def addition(a, b):
+
     return a + b
 
 
 def subtraction(a, b):
+
     return a - b
 
 
 def multiplication(a, b):
+
     return a * b
 
 
 def division(a, b):
+
     return a / b
 
 
@@ -43,7 +49,12 @@ def ask_number(message):
             print("saisie invalide.")
 
 
+def number_a():
+    return ask_number("Entrez le premier nombre :")
 
+
+def number_b():
+    return ask_number("Entrez la second nombre :")
 
 
 if __name__ == '__main__':
