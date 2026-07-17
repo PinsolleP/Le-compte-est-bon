a10c4b0f1afba364ed28627ea4d2d0305045c532
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-07-17 11:27:52 +0200

    Modification de la fonction plates_of_game

--- le_compte_est_bon.py
+++ le_compte_est_bon.py
@@ -3,106 +3,109 @@
 
 import random
 
+initial_plates = None
+
 
 def result_number():
-
     return random.randint(101, 999)
 
 
 def plates_of_game():
 
-    plates = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 25, 50, 75, 100]
-    return random.sample(plates, 6)
+    global initial_plates
+    if initial_plates is None:
+        plates = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 25, 50, 75, 100]
+        initial_plates = random.sample(plates, 6)
+    return initial_plates
 
 
 def addition(a, b):
-
     return a + b
 
 
 def subtraction(a, b):
-
     return a - b
 
 
 def multiplication(a, b):
-
     return a * b
 
 
 def division(a, b):
-
     return a / b
 
 
 def ask_number(message):
-
     while True:
         number = input(message).strip()  # ignore les espaces avant et après la saisie
         if number.lstrip("-").isdigit():  # isdigit() = est numérique
             a = int(number)
             if a in plates_of_game():
                 return a
-            else:
-                print(f"Le nombre doit être dans la liste : {plates_of_game()}")
         else:
-            print("saisie invalide.")
+            print(f"Le nombre doit être dans la liste : {plates_of_game()}")
 
 
 def number_a():
-
     return ask_number("Entrez le premier nombre :")
 
 
 def number_b():
-
     return ask_number("Entrez la second nombre :")
 
 
 def ask_operation():
-
     valid_operation = ["+", "-", "*", "/"]
     while True:
         operate = input("Veuillez choisir une opération : '+', '-', '*', '/' : ").strip()
         if operate in valid_operation:
-            if operate == "+":
-                return operate
+            return operate
         else:
             print("Choix invalide.")
 
 
 def ask_continue(message="Voulez vous continuer les opérations ? (o/n) :"):
-
     answer = ""
     while answer not in ["o", "oui", "n", "non"]:
         answer = input(message).strip().lower()
-        if answer not in ["o","oui", "n", "non"]:
+        if answer not in ["o", "oui", "n", "non"]:
             print("Choix invalide. Veuillez entrer 'o', 'oui', 'n', 'non'.")
     return answer
 
 
 def calculation(a, b, operate):
-
     if operate == "+":
-        result = addition(a, b)
+        return addition(a, b)
     elif operate == "-":
-        result = subtraction(a, b)
+        return subtraction(a, b)
     elif operate == "*":
-        result = multiplication(a, b)
+        return multiplication(a, b)
     else:
-        result = division(a, b)
-    return result
+        return division(a, b)
 
 
-def update_plate(a, b, result):
+def update_plate(a, b, operate):
 
     plates = plates_of_game()
     for plate in (a, b):
         plates.remove(plate)
-    plates.append(result)
+    plates.append(calculation(a, b, operate))
     return plates
+
+
+def game_mechanics():
+    print(f"{plates_of_game()} résultat : {result_number()}")
+    a = number_a()
+    operation = ask_operation()
+    b = number_b()
+    result = calculation(a, b, operation)
+    print(f"Le résultat de l'opération est : {result}")
+    ask_continue()
+    if ask_continue() == "oui" or ask_continue() == "o":
+        update_plate(a, b, operation)
 
 
 
 if __name__ == '__main__':
-    print("Bienvenue pour une partie de : Le compte est bon")
+    print("Bienvenue pour une partie de : Le compte est bon !")
+    game_mechanics()
